4e4c2780d0b730815b860be1b708ef7a3d1f1660
gesticulat <josh.deavin@gmail.com>
2026-08-22 01:18:00 +0100

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        table = str.maketrans(")]}", "([{")
+        stack = []
+
+        for char in s:
+            if char in "([{":
+                stack.append(char)
+            elif len(stack) >= 1 and stack[-1] == char.translate(table):
+                stack.pop()
+            else:
+                return False
+        return len(stack) == 0
+        
